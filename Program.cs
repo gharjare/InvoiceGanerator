0d5a0352fe1edd06e843d61645ad20ff8bce8f91
gharjare <gharjarechetan@gmail.com>
2023-03-04 12:50:53 +0530

    [chetan gharjare] cab invoice

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("welcome to cab invoice generator");
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoice.CalculateFare(2.0, 5);
+            Console.WriteLine($"FARE : {fare}");
         }
     }
 
